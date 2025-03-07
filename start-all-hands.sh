e596da9cb8e6d0646cb7fa6fea877827bd66fbef
fk <krueger.fab@gmail.com>
2025-03-07 12:36:17 +0100

    Fixed OpenHands start script

--- start-all-hands.sh
+++ start-all-hands.sh
@@ -1,8 +1,7 @@
 WORKSPACE_BASE=`realpath .`
 echo "start workspace_base: ${WORKSPACE_BASE}"
-#--rm --pull=always \
 docker run -it \
-  -e SANDBOX_RUNTIME_CONTAINER_IMAGE=docker.io/library/all-hands-custom-arm:0.24 \
+  -e SANDBOX_RUNTIME_CONTAINER_IMAGE=docker.all-hands.dev/all-hands-ai/runtime:0.27-nikolaik \
   -e LOG_ALL_EVENTS=true \
   -e SANDBOX_USER_ID=$(id -u) \
   -e WORKSPACE_MOUNT_PATH=$WORKSPACE_BASE \
@@ -12,4 +11,4 @@ docker run -it \
   -p 3002:3000 \
   --add-host host.docker.internal:host-gateway \
   --name openhands-vitor-fix-meetings \
-  docker.all-hands.dev/all-hands-ai/openhands:0.24
+  docker.all-hands.dev/all-hands-ai/openhands:0.27
